f8acaae761487ef04c0947f1234f2560087122e2
luotao <luotaoyeah@gmail.com>
2018-03-25 21:11:55 +0800

    composing with components: v2.0

--- src/doc/guide/introduction/ComposingWithComponents.vue
+++ src/doc/guide/introduction/ComposingWithComponents.vue
@@ -1,18 +1,41 @@
 <template>
   <div style="padding: 10px;">
-    <todo-item></todo-item>
+    <ol>
+      <todo-item
+        v-for="item in groceries"
+        v-bind:key="item.id"
+        v-bind:todo="item"
+      ></todo-item>
+    </ol>
   </div>
 </template>
 <script>
 import Vue from "vue";
 
 Vue.component("todo-item", {
-  template: "<span>This is a todo.</span>"
+  props: ["todo"],
+  template: "<li>{{todo.text}}</li>"
 });
+
 export default {
   name: "ComposingWithComponents",
   data() {
-    return {};
+    return {
+      groceries: [
+        {
+          id: 0,
+          text: "Vegetables"
+        },
+        {
+          id: 1,
+          text: "Cheese"
+        },
+        {
+          id: 2,
+          text: "Whatever else humans are supposed to eat"
+        }
+      ]
+    };
   }
 };
 </script>
