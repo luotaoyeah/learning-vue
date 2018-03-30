f6957b92f5f876b947b033fddafa0d1bdb460ff9
luotao <luotaoyeah@gmail.com>
2018-03-30 22:18:39 +0800

    template-syntax: interpolation: html

---
+++ src/doc/guide/template-syntax/interpolation/interpolation-html.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>2. Raw HTML</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+<style scoped>
+</style>
--- src/doc/guide/template-syntax/interpolation/interpolation-text.vue
+++ src/doc/guide/template-syntax/interpolation/interpolation-text.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Text</h1>
+    <h1>1. Text</h1>
     <!--
         使用双花括号的语法，花括号里面是 data 对象的属性；
     -->
@@ -8,7 +8,7 @@
     <!--
         使用 v-once 指令，只在第一次解析的时候绑定变量的值；后面更新变量值的时候，模板上绑定的值不会再变化；
     -->
-    <h2>v-once</h2>
+    <h2>1.1. v-once</h2>
     <p v-once>Messasge: {{msg}}</p>
   </div>
 </template>
