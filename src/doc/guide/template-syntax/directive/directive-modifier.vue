85d81efc512eb2dfb78018d120d7069f95100890
luotao <luotaoyeah@gmail.com>
2018-03-31 11:14:16 +0800

    template-syntax: directive: modifiers

--- src/doc/guide/template-syntax/directive/directive-modifier.vue
+++ src/doc/guide/template-syntax/directive/directive-modifier.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <h1>2. Modifiers</h1>
+    <h1>2. Modifiers（修饰符）</h1>
+    <p>
+      <small>
+        <i>
+          修饰符是跟在点（.）后面的特殊的后缀，用来表示一些特殊的绑定行为；
+        </i>
+      </small>
+    </p>
+    <h2>2.1. v-on.prevent</h2>
+    <p>
+      <small>
+        <i>
+          如指令 v-on 的修饰符 prevent 表示在事件回调函数里面调用 e.preventDefault() 方法；
+        </i>
+      </small>
+    </p>
   </div>
 </template>
 <script>
