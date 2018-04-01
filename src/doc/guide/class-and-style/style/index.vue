3609f29bcdce7779a1fc7ef2394e2c9d461ae16b
luotao <luotaoyeah@gmail.com>
2018-04-01 14:22:49 +0800

    style binding: auto prefixing

--- src/doc/guide/class-and-style/style/index.vue
+++ src/doc/guide/class-and-style/style/index.vue
@@ -5,18 +5,21 @@
     <StyleString></StyleString>
     <StyleObject></StyleObject>
     <StyleArray></StyleArray>
+    <AutoPrefixing></AutoPrefixing>
   </div>
 </template>
 <script>
 import StyleString from "./style-string";
 import StyleObject from "./style-object";
 import StyleArray from "./style-array";
+import AutoPrefixing from "./auto-prefixing";
 
 export default {
   components: {
     StyleObject,
     StyleString,
-    StyleArray
+    StyleArray,
+    AutoPrefixing
   }
 };
 </script>
