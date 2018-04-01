b7b722800e1f784315174897f609aaee1e765b4f
luotao <luotaoyeah@gmail.com>
2018-04-01 13:26:41 +0800

    class binding: object syntax

--- src/doc/guide/class-and-style/class/index.vue
+++ src/doc/guide/class-and-style/class/index.vue
@@ -2,6 +2,13 @@
   <div
     style="padding: 10px;"
   >
+    <p>
+      <small>
+        <i>
+          class 和 style 都是普通的 HTML 属性，可以使用 v-bind 来绑定；vue 对它们提供了增强，可以绑定对象和数组；
+        </i>
+      </small>
+    </p>
     <ClassObject></ClassObject>
   </div>
 </template>
