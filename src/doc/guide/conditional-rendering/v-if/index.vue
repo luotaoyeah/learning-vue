2016502331bb35f94c82761e4e3fc06ac31926bb
luotao <luotaoyeah@gmail.com>
2018-04-01 15:06:34 +0800

    v-else, v-else-if

--- src/doc/guide/conditional-rendering/v-if/index.vue
+++ src/doc/guide/conditional-rendering/v-if/index.vue
@@ -3,17 +3,20 @@
     style="padding: 10px;"
   >
     <VIfOnTemplate></VIfOnTemplate>
+    <VElse></VElse>
   </div>
 </template>
 <script>
 import VIfOnTemplate from "./v-if-on-template";
+import VElse from "./v-else";
 
 export default {
   data() {
     return {};
   },
   components: {
-    VIfOnTemplate
+    VIfOnTemplate,
+    VElse
   }
 };
 </script>
---
+++ src/doc/guide/conditional-rendering/v-if/v-else.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>2. v-else, v-else-if</h1>
+    <h2>2.1. v-else</h2>
+    <p>
+      <small>
+        <i>
+          可以在 v-if 或者 v-else-if 后面紧跟一个 v-else；
+        </i>
+      </small>
+    </p>
+    <p v-if="active">Now you see me.</p>
+    <p v-else>Now you don't.</p>
+    <h2>2.2. v-else-if</h2>
+    <p>
+      <small>
+        <i>
+          可以在 v-if 和 v-else 之间使用多个 v-else-if；
+        </i>
+      </small>
+    </p>
+    <p v-if="name === 'A'">A</p>
+    <p v-else-if="name === 'B'">B</p>
+    <p v-else-if="name === 'C'">C</p>
+    <p v-else>D</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      active: true,
+      name: "A"
+    };
+  }
+};
+</script>
+<style scoped>
+</style>
