50a65205f5e6a4d2758b816917cd507895e25541
luotao <luotaoyeah@gmail.com>
2018-04-04 14:44:39 +0800

    form input binding: textarea

--- src/doc/guide/form-input-binding/basic/form-input-multiline-text.vue
+++ src/doc/guide/form-input-binding/basic/form-input-multiline-text.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
     <h1>2. Multiline Text</h1>
+    <textarea
+      style="border-radius: 3px;"
+      type="textarea"
+      rows="3"
+      placeholder="edit me"
+      v-model="message"
+    ></textarea>
+    <p>Message: {{message}}</p>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      message: "hello world"
+    };
   }
 };
 </script>
