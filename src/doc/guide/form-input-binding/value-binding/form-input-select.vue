d1dc7e0194032e6d0beb29916c5f10fa3e1ead88
luotao <luotaoyeah@gmail.com>
2018-04-04 16:02:37 +0800

    form input binding: value binding: select

--- src/doc/guide/form-input-binding/value-binding/form-input-select.vue
+++ src/doc/guide/form-input-binding/value-binding/form-input-select.vue
@@ -1,12 +1,41 @@
 <template>
   <div>
     <h1>3. Select Options</h1>
+    <p>
+      <small>
+        <i>
+          下拉列表绑定到对象；
+        </i>
+      </small>
+    </p>
+    <select
+      v-model="selected"
+    >
+      <option
+        value=""
+        disabled
+      >请选择：
+      </option>
+      <option :value="tom">tom</option>
+      <option :value="cat">cat</option>
+    </select>
+    <p>Selected: {{selected}}</p>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      selected: "",
+      tom: {
+        name: "tom",
+        age: 18
+      },
+      cat: {
+        name: "cat",
+        age: 20
+      }
+    };
   }
 };
 </script>
