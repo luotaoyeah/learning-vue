05c8c6f6e80fe4bcb5c11e918c237c31d9cb07ff
luotao <luotaoyeah@gmail.com>
2018-04-04 15:54:24 +0800

    form input binding: value binding: checkbox

--- src/doc/guide/form-input-binding/value-binding/form-input-checkbox.vue
+++ src/doc/guide/form-input-binding/value-binding/form-input-checkbox.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
     <h1>1. Checkbox</h1>
+    <p>
+      <small>
+        <i>
+          checkbox 很特别，因为 v-model 绑定的不是 checkbox 的 value 属性，
+          而是 selected 属性；
+          我们需要通过 true-value 和 false-value 来设置动态绑定的值；
+        </i>
+      </small>
+    </p>
+    <input
+      id="checkbox01"
+      type="checkbox"
+      v-model="selected"
+      true-value="yes"
+      false-value="no"
+    >
+    <label for="checkbox01">yes or no</label>
+    <p>Selected: {{selected}}</p>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      selected: "yes"
+    };
   }
 };
 </script>
--- src/doc/guide/form-input-binding/value-binding/index.vue
+++ src/doc/guide/form-input-binding/value-binding/index.vue
@@ -2,6 +2,22 @@
   <div
     style="padding: 10px;"
   >
+    <p>
+      <small>
+        <i>
+          默认情况下，v-model 绑定的数据属性只可能是静态的，
+          即绑定的是表单元素的 value 值：
+          <ul>
+            <li>string（radio，select）</li>
+            <li>boolean（checkbox）</li>
+          </ul>
+          举例来说，对于 checkbox 来说，如果勾选则绑定值为 true，
+          如果不勾选则绑定值为 false；如何实现勾选时绑定值为 'yes'，
+          不勾选时绑定值为 'no' 这样的动态绑定?
+          很简单，使用 v-bind 对表单元素的 value 属性设置动态绑定就可以；
+        </i>
+      </small>
+    </p>
     <FormInputCheckbox></FormInputCheckbox>
     <FormInputRadio></FormInputRadio>
     <FormInputSelect></FormInputSelect>
