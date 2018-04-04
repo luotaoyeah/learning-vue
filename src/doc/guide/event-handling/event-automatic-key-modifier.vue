392248a8ad5e8a7fb012ff491919c7d78f8ef7d2
luotao <luotaoyeah@gmail.com>
2018-04-04 11:01:04 +0800

    event handling: automatic key modifiers

--- src/doc/guide/event-handling/event-automatic-key-modifier.vue
+++ src/doc/guide/event-handling/event-automatic-key-modifier.vue
@@ -1,5 +1,19 @@
 <template>
-  <div><h1>6. Automatic Key Modifier</h1>
+  <div>
+    <h1>6. Automatic Key Modifier</h1>
+    <p>
+      <small>
+        <i>
+          可以直接使用 KeyboardEvent.key 作为键盘事件修饰符；
+          使用的时候，将 KeyboardEvent.key 转换为 kebab-case；
+        </i>
+      </small>
+    </p>
+    <input
+      style="padding: 7px;border-radius: 3px;"
+      type="text"
+      @keyup.control="handleKeyUp01"
+    />
   </div>
 </template>
 <script>
@@ -7,7 +21,13 @@ export default {
   data() {
     return {};
   },
-  methods: {}
+  methods: {
+    handleKeyUp01(e) {
+      if (e) {
+        alert(e.code);
+      }
+    }
+  }
 };
 </script>
 <style scoped>
