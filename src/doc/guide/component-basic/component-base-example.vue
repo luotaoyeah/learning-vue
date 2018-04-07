1c7905c0c84c1fd3919343a22e0c9d732f6a40fa
luotao <luotaoyeah@gmail.com>
2018-04-07 13:22:51 +0800

    component basic: base example

--- src/doc/guide/component-basic/component-base-example.vue
+++ src/doc/guide/component-basic/component-base-example.vue
@@ -1,9 +1,30 @@
 <template>
   <div>
     <h1>1. Base Example</h1>
+    <small>
+      <ul>
+        <li>组件就是可以重用的 vue 实例；</li>
+        <li>组件都有 name；</li>
+        <li>使用时，把组件当做一个自定义的 DOM 元素来使用；</li>
+      </ul>
+    </small>
+    <ButtonCounter></ButtonCounter>
+    <button-counter></button-counter>
   </div>
 </template>
 <script>
+import Vue from "vue";
+
+Vue.component("ButtonCounter", {
+  template:
+    '<button v-on:click="count++">You clicked {{count}} times.</button>',
+  data() {
+    return {
+      count: 0
+    };
+  }
+});
+
 export default {
   data() {
     return {};
