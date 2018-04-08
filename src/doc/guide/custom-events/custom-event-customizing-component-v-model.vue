11a4bb557b93d6980fa7fd69567cdbada2eb9c7f
luotao <luotaoyeah@gmail.com>
2018-04-08 19:04:30 +0800

    custom events: customizing component v-model

--- src/doc/guide/custom-events/custom-event-customizing-component-v-model.vue
+++ src/doc/guide/custom-events/custom-event-customizing-component-v-model.vue
@@ -1,12 +1,45 @@
 <template>
   <div>
     <h1>2. Customizing Component v-model</h1>
+    <small>
+      <ul>
+        <li>
+          默认情况下，v-model 使用的 props 是 value，
+          使用的事件名称是 input；
+        </li>
+        <li>可以使用组件配置项 model 来自定义这两个配置；</li>
+      </ul>
+    </small>
+    <MyCheckbox v-model="checked"></MyCheckbox>
   </div>
 </template>
 <script>
+import Vue from "vue";
+
+Vue.component("MyCheckbox", {
+  template: `
+<label for="my-checkbox">
+  MyCheckbox:
+  <input id="my-checkbox" type="checkbox" v-bind:checked="checked02" v-on:change="$emit('change02', $event.target.checked)">
+</label>
+  `,
+  props: {
+    checked02: {
+      type: Boolean,
+      required: true
+    }
+  },
+  model: {
+    prop: "checked02",
+    event: "change02"
+  }
+});
+
 export default {
   data() {
-    return {};
+    return {
+      checked: true
+    };
   }
 };
 </script>
