75a34252c2e9f0cd4d669d7991d26818e75b2171
luotao <luotaoyeah@gmail.com>
2018-04-12 21:36:37 +0800

    handling edge cases: element & component access: access child component

--- src/doc/guide/handling-edge-cases/edge-case-element-component-access.vue
+++ src/doc/guide/handling-edge-cases/edge-case-element-component-access.vue
@@ -33,13 +33,62 @@
       </ul>
     </small>
     <h2>1.3. Accessing Child Component Instance</h2>
+    <small>
+      <ul>
+        <li>
+          虽然 vue 提供了 props 和 event 用来进行父子组件之间的交互，
+          但是有时候，父组件需要在 JS 代码中直接访问子组件的数据和方法；
+        </li>
+        <li>
+          可以通过给子组件设置 ref 属性，
+          然后父组件就可以通过子组件的 ref 属性，获取到子组件实例的引用；
+          <p>
+            <button @click="$refs.myInputRef01.focus()">focus input</button>
+            <MyInput ref="myInputRef01"></MyInput>
+          </p>
+        </li>
+        <li>
+          如果 ref 和 v-for 一起使用，
+          通过 ref 获取到的是子组件实例数组；
+          <p>
+            <button @click="getRefs">get refs</button>
+            <MyInput
+              ref="myInputRef02"
+              v-for="(item, itemIndex) in 3"
+              :key="'my_input_02_' + itemIndex"
+            ></MyInput>
+          </p>
+        </li>
+      </ul>
+    </small>
     <h2>1.4. Dependency Injection</h2>
+
   </div>
 </template>
 <script>
 export default {
   data() {
     return {};
+  },
+  methods: {
+    getRefs() {
+      console.log("$refs.myInputRef02", this.$refs.myInputRef02);
+    }
+  },
+  components: {
+    MyInput: {
+      template: `
+        <input
+          type="text"
+          style="border-radius: 3px;padding: 7px;"
+          ref="inputRef"
+        />`,
+      methods: {
+        focus() {
+          this.$refs.inputRef.focus();
+        }
+      }
+    }
   }
 };
 </script>
