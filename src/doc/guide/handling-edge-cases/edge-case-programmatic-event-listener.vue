11bbefb5347109489ac232124a2f16af9bb67e7d
luotao <luotaoyeah@gmail.com>
2018-04-13 22:23:19 +0800

    handling edge cases: programmatic event listeners

--- src/doc/guide/handling-edge-cases/edge-case-programmatic-event-listener.vue
+++ src/doc/guide/handling-edge-cases/edge-case-programmatic-event-listener.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
     <h1>2. Programmatic Event Listeners</h1>
+    <small>
+      <ul>
+        <li>
+          vue 提供了 3 个跟事件相关的实例方法：
+          <ul>
+            <li>$on: 添加事件监听；</li>
+            <li>$once: 添加单次事件监听；</li>
+            <li>$off: 移除事件监听；</li>
+          </ul>
+        </li>
+        <li>
+          一般情况下，我们不需要用到这 3 个方法；
+        </li>
+      </ul>
+    </small>
   </div>
 </template>
 <script>
