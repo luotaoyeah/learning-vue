b29d2449fe882ae841bb028f33ec686251376617
luotao <luotaoyeah@gmail.com>
2018-04-14 09:06:21 +0800

    handling edge cases: programmatic event listeners

--- src/doc/guide/handling-edge-cases/edge-case-programmatic-event-listener.vue
+++ src/doc/guide/handling-edge-cases/edge-case-programmatic-event-listener.vue
@@ -13,15 +13,72 @@
         </li>
         <li>
           一般情况下，我们不需要用到这 3 个方法；
+          <MyComponent ref="myComponentRef01"></MyComponent>
+        </li>
+        <li>
+          使用 $on 监听子组件的事件，需要用子组件的实例对象来调用 $on 方法；
+          即
+          <i>'监听谁的事件，就由谁来调用 $on 方法'</i>
+          ；
         </li>
       </ul>
     </small>
   </div>
 </template>
 <script>
+import Vue from "vue";
+
+Vue.component("MyComponent", {
+  template: `
+<div>
+  <button @click="$emit('click-me')">click me</button>
+  <ul>
+    <li
+      v-for="(message, messageIndex) in messages"
+      :key="'message_' + messageIndex"
+    >{{message}}</li>
+  </ul>
+</div>`,
+  data() {
+    return {
+      messages: []
+    };
+  },
+  beforeCreate() {
+    const vm = this;
+    vm.$once("hook:created", () => {
+      vm.messages.push("created");
+    });
+    vm.$once("hook:beforeMount", () => {
+      vm.messages.push("beforeMount");
+    });
+    vm.$once("hook:mounted", () => {
+      vm.messages.push("mounted");
+    });
+    vm.$once("hook:beforeUpdate", () => {
+      vm.messages.push("beforeUpdate");
+    });
+    vm.$once("hook:updated", () => {
+      vm.messages.push("updated");
+    });
+    vm.$once("hook:beforeDestroy", () => {
+      vm.messages.push("beforeDestroy");
+    });
+    vm.$once("hook:destroyed", () => {
+      vm.messages.push("destroyed");
+    });
+  }
+});
+
 export default {
   data() {
     return {};
+  },
+  mounted() {
+    const vm = this;
+    vm.$refs.myComponentRef01.$on("click-me", () => {
+      alert("click-me on MyComopnent");
+    });
   }
 };
 </script>
