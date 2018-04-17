050ca480c2b9893cce8e6d0cf9b3d7d0ccf80d29
luotao <luotaoyeah@gmail.com>
2018-04-17 21:33:10 +0800

    mixins: basics

--- src/doc/guide/19-mixins/01-basics.vue
+++ src/doc/guide/19-mixins/01-basics.vue
@@ -1,7 +1,38 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li>
+        mixin 对象可以包含任意的组件选项；
+      </li>
+      <li>
+        当组件使用 mixin 时，
+        mixin 的选项会被混入组件自己的选项；
+        <p>
+          <MyComponent></MyComponent>
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
+import Vue from "vue";
+
+const MyMixin = {
+  created() {
+    this.hello();
+  },
+  methods: {
+    hello() {
+      alert("MyMixin.hello()");
+    }
+  }
+};
+
+Vue.component("MyComponent", {
+  template: "<el-alert :closable='false'>my component</el-alert>",
+  mixins: [MyMixin]
+});
+
 export default {
   data() {
     return {};
