39d7e8229e2e1c10f9d84cbe3558c7faafbae300
luotao <luotaoyeah@gmail.com>
2018-04-18 22:19:03 +0800

    custom directive: function shorthand

--- src/doc/guide/20-custom-directives/04-function-shorthand.vue
+++ src/doc/guide/20-custom-directives/04-function-shorthand.vue
@@ -1,10 +1,32 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li>
+        如果期望在 bind 和 update 两个钩子函数中执行同样的逻辑，
+        并且不关心其他的钩子函数，
+        就可以使用函数简写的形式；
+        <p>
+          <el-input v-model="message"></el-input>
+        </p>
+        <p
+          style="border: 2px solid #dddddd;border-radius: 3px;padding: 7px;"
+          v-demo-directive-200401="message"
+        >
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
+import Vue from "vue";
+
+Vue.directive("demo-directive-200401", (el, binding) => {
+  el.innerHTML = String(binding.value).toUpperCase();
+});
+
 export default {
   data() {
-    return {};
+    return { message: "" };
   },
   components: {}
 };
