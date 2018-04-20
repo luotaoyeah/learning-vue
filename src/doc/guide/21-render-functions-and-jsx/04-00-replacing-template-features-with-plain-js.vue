6b025798aef7b19271c23a1c279abab6d6fd3d75
luotao <luotaoyeah@gmail.com>
2018-04-20 08:59:18 +0800

    render function: replacing template features with plain js

---
+++ src/doc/guide/21-render-functions-and-jsx/04-00-replacing-template-features-with-plain-js.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <C0401></C0401>
+    <C0402></C0402>
+    <C0403></C0403>
+    <C0404></C0404>
+  </div>
+</template>
+<script>
+import C0401 from "./04-01-vif-vfor";
+import C0402 from "./04-02-vmodel";
+import C0403 from "./04-03-event-key-modifiers";
+import C0404 from "./04-04-slots";
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    C0401,
+    C0402,
+    C0403,
+    C0404
+  }
+};
+</script>
