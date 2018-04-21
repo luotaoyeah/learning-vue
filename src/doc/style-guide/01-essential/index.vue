13d55f1588eaf72ebab1f9fe5ad2a307353a55ea
luotao <luotaoyeah@gmail.com>
2018-04-21 21:19:17 +0800

    style guide: keyed v-for

--- src/doc/style-guide/01-essential/index.vue
+++ src/doc/style-guide/01-essential/index.vue
@@ -12,12 +12,16 @@
     <el-tab-pane label="Prop Definitions">
       <C03></C03>
     </el-tab-pane>
+    <el-tab-pane label="Keyed v-for">
+      <C04></C04>
+    </el-tab-pane>
   </el-tabs>
 </template>
 <script>
 import C01 from "./01-multiword-component-names";
 import C02 from "./02-component-data";
 import C03 from "./03-props-definitions";
+import C04 from "./04-keyed-vfor";
 
 export default {
   data() {
@@ -26,7 +30,8 @@ export default {
   components: {
     C01,
     C02,
-    C03
+    C03,
+    C04
   }
 };
 </script>
