d79bb4236c9f95e022931b1e81d9d135ae6099a5
luotao <luotaoyeah@gmail.com>
2018-04-21 14:05:21 +0800

    guide: reactivity in depth: toc

--- src/doc/guide/24-reactivity-in-depth/index.vue
+++ src/doc/guide/24-reactivity-in-depth/index.vue
@@ -7,9 +7,20 @@
   </el-tabs>
 </template>
 <script>
+import C01 from "./01-how-changes-are-tracked";
+import C02 from "./02-change-detection-caveats";
+import C03 from "./03-declaring-reactive-properties";
+import C04 from "./04-async-update-qunee";
+
 export default {
   data() {
     return {};
+  },
+  components: {
+    C01,
+    C02,
+    C03,
+    C04
   }
 };
 </script>
