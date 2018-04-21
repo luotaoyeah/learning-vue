658792a11c4a1dd05fd568b4640528c88fa98b99
luotao <luotaoyeah@gmail.com>
2018-04-21 19:23:36 +0800

    style guide: multi-word component names

--- src/doc/style-guide/01-essential/index.vue
+++ src/doc/style-guide/01-essential/index.vue
@@ -3,15 +3,21 @@
     tab-position="left"
     style="height: 100%;"
   >
-    <el-tab-pane label="1.1."></el-tab-pane>
+    <el-tab-pane label="Muli-word component names">
+      <C01></C01>
+    </el-tab-pane>
   </el-tabs>
 </template>
 <script>
+import C01 from "./01-multiword-component-names";
+
 export default {
   data() {
     return {};
   },
-  components: {}
+  components: {
+    C01
+  }
 };
 </script>
 <style scoped>
