245c851c441f3db2b77403f1c11986d242ab2d49
luotao <luotaoyeah@gmail.com>
2018-04-21 19:50:40 +0800

    style guide: prop definitions

--- src/doc/style-guide/01-essential/index.vue
+++ src/doc/style-guide/01-essential/index.vue
@@ -9,11 +9,15 @@
     <el-tab-pane label="Component data">
       <C02></C02>
     </el-tab-pane>
+    <el-tab-pane label="Prop Definitions">
+      <C03></C03>
+    </el-tab-pane>
   </el-tabs>
 </template>
 <script>
 import C01 from "./01-multiword-component-names";
 import C02 from "./02-component-data";
+import C03 from "./03-props-definitions";
 
 export default {
   data() {
@@ -21,7 +25,8 @@ export default {
   },
   components: {
     C01,
-    C02
+    C02,
+    C03
   }
 };
 </script>
