499568173aec9392c59ff19b6e625aef3939c803
luotao <luotaoyeah@gmail.com>
2018-04-22 10:19:31 +0800

    style guide: single-file component filename casing

--- src/doc/style-guide/02-strongly-recommend/index.vue
+++ src/doc/style-guide/02-strongly-recommend/index.vue
@@ -6,17 +6,22 @@
     <el-tab-pane label="Component files">
       <C01></C01>
     </el-tab-pane>
+    <el-tab-pane label="Single-file component filename casing">
+      <C02></C02>
+    </el-tab-pane>
   </el-tabs>
 </template>
 <script>
 import C01 from "./01-component-files";
+import C02 from "./02-sfc-filename-casing";
 
 export default {
   data() {
     return {};
   },
   components: {
-    C01
+    C01,
+    C02
   }
 };
 </script>
