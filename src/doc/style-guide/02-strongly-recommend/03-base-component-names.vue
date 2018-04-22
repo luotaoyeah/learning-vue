8c24f12eb6b3a9697f99a4bb48dc9a292f87cd2c
luotao <luotaoyeah@gmail.com>
2018-04-22 10:25:18 +0800

    style guide: base component names

---
+++ src/doc/style-guide/02-strongly-recommend/03-base-component-names.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>
+      所有的基本组件的文件名，
+      都应该使用一个特定的前缀，
+      比如：Base，App，V；
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+<style scoped>
+</style>
--- src/doc/style-guide/02-strongly-recommend/index.vue
+++ src/doc/style-guide/02-strongly-recommend/index.vue
@@ -9,11 +9,15 @@
     <el-tab-pane label="Single-file component filename casing">
       <C02></C02>
     </el-tab-pane>
+    <el-tab-pane label="Base component names">
+      <C03></C03>
+    </el-tab-pane>
   </el-tabs>
 </template>
 <script>
 import C01 from "./01-component-files";
 import C02 from "./02-sfc-filename-casing";
+import C03 from "./03-base-component-names";
 
 export default {
   data() {
@@ -21,7 +25,8 @@ export default {
   },
   components: {
     C01,
-    C02
+    C02,
+    C03
   }
 };
 </script>
