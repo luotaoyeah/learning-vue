ceb32b5f2fd6437590f5c98304053cebb94af0f6
luotao <luotaoyeah@gmail.com>
2018-04-23 22:13:06 +0800

    style guide: component options order

--- src/doc/style-guide/03-recommend/index.vue
+++ src/doc/style-guide/03-recommend/index.vue
@@ -3,15 +3,21 @@
     tab-position="left"
     style="height: 100%;"
   >
-    <el-tab-pane label="1.1."></el-tab-pane>
+    <el-tab-pane label="Component options order">
+      <C01></C01>
+    </el-tab-pane>
   </el-tabs>
 </template>
 <script>
+import C01 from "./01-component-options-order";
+
 export default {
   data() {
     return {};
   },
-  components: {}
+  components: {
+    C01
+  }
 };
 </script>
 <style scoped>
