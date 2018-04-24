d67057517b7be0885629275ac9166aa2b45b11e1
luotao <luotaoyeah@gmail.com>
2018-04-24 21:29:24 +0800

    style guide: element selector with scoped

--- src/doc/style-guide/04-use-with-caution/index.vue
+++ src/doc/style-guide/04-use-with-caution/index.vue
@@ -6,17 +6,22 @@
     <el-tab-pane label="v-if / v-else with key">
       <C01></C01>
     </el-tab-pane>
+    <el-tab-pane label="Element selector with scoped">
+      <C02></C02>
+    </el-tab-pane>
   </el-tabs>
 </template>
 <script>
 import C01 from "./01-vif-with-key";
+import C02 from "./02-element-selector-with-scoped";
 
 export default {
   data() {
     return {};
   },
   components: {
-    C01
+    C01,
+    C02
   }
 };
 </script>
