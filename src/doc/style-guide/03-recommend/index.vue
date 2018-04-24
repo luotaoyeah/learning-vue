ec8436abde62a3bbccb74ca815ef962b2b59dfe6
luotao <luotaoyeah@gmail.com>
2018-04-24 21:13:16 +0800

    style guide: element attribute order

--- src/doc/style-guide/03-recommend/index.vue
+++ src/doc/style-guide/03-recommend/index.vue
@@ -6,17 +6,22 @@
     <el-tab-pane label="Component options order">
       <C01></C01>
     </el-tab-pane>
+    <el-tab-pane label="Element attribute order">
+      <C02></C02>
+    </el-tab-pane>
   </el-tabs>
 </template>
 <script>
 import C01 from "./01-component-options-order";
+import C02 from "./02-element-attribute-order";
 
 export default {
   data() {
     return {};
   },
   components: {
-    C01
+    C01,
+    C02
   }
 };
 </script>
