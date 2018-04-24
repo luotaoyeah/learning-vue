45ab49d28794a4fa5fbdac4992a2cee52ba56392
luotao <luotaoyeah@gmail.com>
2018-04-24 21:18:30 +0800

    style guide: single-file component order

---
+++ src/doc/style-guide/03-recommend/03-sfc-element-order.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>
+      单文件组件中，
+      &lt;script&gt;，&lt;template&gt;，&lt;style&gt; 的顺序应该保持一致，
+      且 &lt;style&gt; 应该放在最后；
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+<style scoped>
+</style>
--- src/doc/style-guide/03-recommend/index.vue
+++ src/doc/style-guide/03-recommend/index.vue
@@ -9,11 +9,15 @@
     <el-tab-pane label="Element attribute order">
       <C02></C02>
     </el-tab-pane>
+    <el-tab-pane label="Single-file component element order">
+      <C03></C03>
+    </el-tab-pane>
   </el-tabs>
 </template>
 <script>
 import C01 from "./01-component-options-order";
 import C02 from "./02-element-attribute-order";
+import C03 from "./03-sfc-element-order";
 
 export default {
   data() {
@@ -21,7 +25,8 @@ export default {
   },
   components: {
     C01,
-    C02
+    C02,
+    C03
   }
 };
 </script>
