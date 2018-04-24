85b9916f3c0eaa5c36b697eb2c49db67ce19fbd5
luotao <luotaoyeah@gmail.com>
2018-04-24 21:36:44 +0800

    style guide: non-flux state management

---
+++ src/doc/style-guide/04-use-with-caution/04-non-flux-state-management.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>
+      使用 vuex 来管理全局状态，
+      而不要使用 this.$root 或者 global event bus；
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+<style scoped>
+</style>
--- src/doc/style-guide/04-use-with-caution/index.vue
+++ src/doc/style-guide/04-use-with-caution/index.vue
@@ -12,12 +12,16 @@
     <el-tab-pane label="Implicit parent-child communication">
       <C03></C03>
     </el-tab-pane>
+    <el-tab-pane label="Non-flux state management">
+      <C04></C04>
+    </el-tab-pane>
   </el-tabs>
 </template>
 <script>
 import C01 from "./01-vif-without-key";
 import C02 from "./02-element-selector-with-scoped";
 import C03 from "./03-implicit-parent-child-communication";
+import C04 from "./04-non-flux-state-management";
 
 export default {
   data() {
@@ -26,7 +30,8 @@ export default {
   components: {
     C01,
     C02,
-    C03
+    C03,
+    C04
   }
 };
 </script>
