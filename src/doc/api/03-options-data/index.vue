caa51d710e9c5f126c182bce462713d8c5e87e16
luotao <luotaoyeah@gmail.com>
2018-04-25 16:50:25 +0800

    api: options / data

--- src/doc/api/03-options-data/index.vue
+++ src/doc/api/03-options-data/index.vue
@@ -4,15 +4,45 @@
     style="height: 100%;"
   >
     <el-tab-pane label="1">
+      <C01></C01>
+    </el-tab-pane>
+    <el-tab-pane label="1">
+      <C02></C02>
+    </el-tab-pane>
+    <el-tab-pane label="1">
+      <C03></C03>
+    </el-tab-pane>
+    <el-tab-pane label="1">
+      <C04></C04>
+    </el-tab-pane>
+    <el-tab-pane label="1">
+      <C05></C05>
+    </el-tab-pane>
+    <el-tab-pane label="1">
+      <C06></C06>
     </el-tab-pane>
   </el-tabs>
 </template>
 <script>
+import C01 from "./01-data";
+import C02 from "./02-props";
+import C03 from "./03-propsData";
+import C04 from "./04-computed";
+import C05 from "./05-methods";
+import C06 from "./06-watch";
+
 export default {
   data() {
     return {};
   },
-  components: {}
+  components: {
+    C01,
+    C02,
+    C03,
+    C04,
+    C05,
+    C06
+  }
 };
 </script>
 <style scoped>
