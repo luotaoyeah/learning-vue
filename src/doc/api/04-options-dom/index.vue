323f14cf016c287daa9f41e443571f0bbf873faf
luotao <luotaoyeah@gmail.com>
2018-04-25 16:53:02 +0800

    api: options / dom

--- src/doc/api/04-options-dom/index.vue
+++ src/doc/api/04-options-dom/index.vue
@@ -3,16 +3,36 @@
     tab-position="left"
     style="height: 100%;"
   >
-    <el-tab-pane label="1">
+    <el-tab-pane label="el">
+      <C01></C01>
+    </el-tab-pane>
+    <el-tab-pane label="template">
+      <C02></C02>
+    </el-tab-pane>
+    <el-tab-pane label="render">
+      <C03></C03>
+    </el-tab-pane>
+    <el-tab-pane label="renderError">
+      <C04></C04>
     </el-tab-pane>
   </el-tabs>
 </template>
 <script>
+import C01 from "./01-el";
+import C02 from "./02-template";
+import C03 from "./03-render";
+import C04 from "./04-renderError";
+
 export default {
   data() {
     return {};
   },
-  components: {}
+  components: {
+    C01,
+    C02,
+    C03,
+    C04
+  }
 };
 </script>
 <style scoped>
