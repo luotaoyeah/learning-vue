61e98b1085e97ca3a4b0027181e22d6878bc03fb
luotao <luotaoyeah@gmail.com>
2018-04-25 21:57:06 +0800

    global config: silent

--- src/doc/api/01-global-config/01-silent.vue
+++ src/doc/api/01-global-config/01-silent.vue
@@ -1,10 +1,26 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li>类型：boolean</li>
+      <li>默认：false</li>
+      <li>
+        描述：阻止 vue 在控制台打印日志和警告；
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
+import Vue from "vue";
+
 export default {
   data() {
     return {};
+  },
+  created() {
+    Vue.config.silent = true;
+  },
+  beforeDestroy() {
+    Vue.config.silent = false;
   },
   components: {}
 };
