278023b63ce98afe68f9ea4879a8610f088ca3d4
luotao <luotaoyeah@gmail.com>
2018-04-26 12:01:55 +0800

    api: instance methods: data

--- src/doc/api/10-instance-methods-data/index.vue
+++ src/doc/api/10-instance-methods-data/index.vue
@@ -3,16 +3,31 @@
     tab-position="left"
     style="height: 100%;"
   >
-    <el-tab-pane label="1">
+    <el-tab-pane label="vm.$watch">
+      <C01></C01>
+    </el-tab-pane>
+    <el-tab-pane label="vm.$set">
+      <C02></C02>
+    </el-tab-pane>
+    <el-tab-pane label="vm.$delete">
+      <C03></C03>
     </el-tab-pane>
   </el-tabs>
 </template>
 <script>
+import C01 from "./01-watch";
+import C02 from "./02-set";
+import C03 from "./03-delete";
+
 export default {
   data() {
     return {};
   },
-  components: {}
+  components: {
+    C01,
+    C02,
+    C03
+  }
 };
 </script>
 <style scoped>
