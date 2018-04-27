f815013681b7954cd14b7a746f6b6a353b67523e
luotao <luotaoyeah@gmail.com>
2018-04-27 17:49:30 +0800

    options: propsData

--- src/doc/api/03-options-data/03-propsData.vue
+++ src/doc/api/03-options-data/03-propsData.vue
@@ -1,10 +1,54 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li>类型：{ [key: string]: any }</li>
+      <li>
+        描述：设置 props 属性值；
+        只有在使用 new 创建组件实例的时候有效；
+        <p>
+          <small>
+            这个属性主要是用来进行单元测试；
+          </small>
+        </p>
+        <ul>
+          <li>
+            <span :id="id01"></span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
+import Vue from "vue";
+
+const id01 = Vue._.uniqueId("span_");
+
+const C01 = Vue.extend({
+  template: '<ElButton @click="handleClick01">click me</ElButton>',
+  props: ["message"],
+  methods: {
+    handleClick01() {
+      const vm = this;
+      vm.$alert(vm.message, "vm.message", {
+        showClose: false
+      });
+    }
+  }
+});
+
 export default {
   data() {
-    return {};
+    return { id01 };
+  },
+  mounted() {
+    const vm = this;
+    const c01 = new C01({
+      propsData: {
+        message: "hello"
+      }
+    });
+    c01.$mount(`#${vm.id01}`);
   },
   components: {}
 };
