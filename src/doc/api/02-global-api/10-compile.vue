57442b75f268cc499e6cad3afbf8b901738c0dbd
luotao <luotaoyeah@gmail.com>
2018-04-27 16:09:21 +0800

    global api: compile

--- src/doc/api/02-global-api/10-compile.vue
+++ src/doc/api/02-global-api/10-compile.vue
@@ -1,10 +1,46 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li>
+        描述：将字符串模板编译为渲染函数；
+      </li>
+      <li>
+        参数：
+        <ul>
+          <li>{ string } template</li>
+        </ul>
+      </li>
+    </ul>
+    <span :id="id02"></span>
+  </div>
 </template>
 <script>
+import Vue from "vue";
+
+const id01 = Vue._.uniqueId("C");
+const id02 = Vue._.uniqueId("span_");
+
+Vue.component(id01, {
+  render: Vue.compile("<ElButton @click='handleClick01'>click me</ElButton>")
+    .render,
+  methods: {
+    handleClick01() {
+      alert(`${id01}.handleClick01()`);
+    }
+  }
+});
+
 export default {
   data() {
-    return {};
+    return {
+      id01,
+      id02
+    };
+  },
+  mounted() {
+    const vm = this;
+    const C01 = Vue.component(id01);
+    new C01().$mount(`#${vm.id02}`);
   },
   components: {}
 };
