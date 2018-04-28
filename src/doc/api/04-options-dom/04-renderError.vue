2b444772bdac641c351a2a55fade0bedaf5fbeb4
luotao <luotaoyeah@gmail.com>
2018-04-28 11:57:18 +0800

    options: renderError

--- src/doc/api/04-options-dom/04-renderError.vue
+++ src/doc/api/04-options-dom/04-renderError.vue
@@ -1,10 +1,70 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li>
+        类型：(h: () => VNode, error: Error) => VNode
+      </li>
+      <li>
+        描述：如果渲染函数报错，
+        则将这个函数作为替代的渲染函数，
+        同时将渲染函数报的错作为第二个参数传入；
+        <ul>
+          <li>只在开发环境有效；</li>
+          <li>
+            <p>
+              <span :id="id01"></span>
+            </p>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
+import Vue from "vue";
+
+const id01 = Vue._.uniqueId("span");
+
+const C01 = Vue.extend({
+  render(h) {
+    throw new Error("error within render()");
+  },
+  renderError(h, error) {
+    return h(
+      "ElAlert",
+      {
+        props: {
+          title: "",
+          closable: false
+        }
+      },
+      [
+        h(
+          "pre",
+          {
+            style: {
+              color: "red"
+            }
+          },
+          [error.stack]
+        )
+      ]
+    );
+  }
+});
+
 export default {
   data() {
-    return {};
+    return {
+      id01
+    };
+  },
+  mounted() {
+    const vm = this;
+
+    new C01({
+      el: `#${id01}`
+    });
   },
   components: {}
 };
