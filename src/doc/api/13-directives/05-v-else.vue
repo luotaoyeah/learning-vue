0fa84f3626760e5a41200a3b7ff07a7986694c72
luotao <luotaoyeah@gmail.com>
2018-04-28 15:37:11 +0800

    directives: v-else

--- src/doc/api/13-directives/05-v-else.vue
+++ src/doc/api/13-directives/05-v-else.vue
@@ -1,10 +1,26 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li>
+        描述：条件分支；
+        跟 v-if 或者 v-else-if 一起使用；
+        <p>
+          <ElSwitch v-model="show01"/>
+        </p>
+        <p>
+          <ElTag v-if="show01">YES</ElTag>
+          <ElTag v-else>NO</ElTag>
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      show01: true
+    };
   },
   components: {}
 };
--- src/doc/api/13-directives/06-v-else-if.vue
+++ src/doc/api/13-directives/06-v-else-if.vue
@@ -1,5 +1,12 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li>
+        描述：条件分支；
+        跟 v-if 或者 v-else 一起使用；
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
