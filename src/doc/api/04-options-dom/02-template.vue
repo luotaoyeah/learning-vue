c934a3b59b19426700327706ff7c540be57f0756
luotao <luotaoyeah@gmail.com>
2018-04-28 11:33:04 +0800

    options: template

--- src/doc/api/04-options-dom/02-template.vue
+++ src/doc/api/04-options-dom/02-template.vue
@@ -1,10 +1,72 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li>
+        类型：string
+      </li>
+      <li>
+        描述：字符串模板；
+        <ul>
+          <li>
+            模板会替换掉组件的挂载元素中的内容；
+            <p>
+              <span :id="id01">
+                <span>mount element content</span>
+              </span>
+            </p>
+          </li>
+          <li>
+            <!--TODO 待验证-->
+            如果模板中声明了 slot，
+            则组件的挂载元素中的内容，
+            会被作为分发内容（？待验证）；
+            <p>
+              <span
+                :id="id02"
+              >
+                <span>mount element content</span>
+              </span>
+            </p>
+          </li>
+          <li>
+            如果属性值以 # 开头，
+            表示元素选择器；
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
+import Vue from "vue";
+
+const id01 = Vue._.uniqueId("span");
+const id02 = Vue._.uniqueId("span");
+
+const C01 = Vue.extend({
+  template: "<ElButton>C01</ElButton>"
+});
+
+const C02 = Vue.extend({
+  template: "<ElButton>C02<br/><slot></slot></ElButton>"
+});
+
 export default {
   data() {
-    return {};
+    return {
+      id01,
+      id02
+    };
+  },
+  mounted() {
+    const vm = this;
+
+    new C01({
+      el: `#${id01}`
+    });
+
+    const c02 = new C02();
+    c02.$mount(`#${id02}`);
   },
   components: {}
 };
