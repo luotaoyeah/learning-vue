7e48031b53e0be85e36d32f8e873071b43279090
luotao <luotaoyeah@gmail.com>
2018-04-28 14:57:51 +0800

    directives: v-show

--- src/doc/api/13-directives/03-v-show.vue
+++ src/doc/api/13-directives/03-v-show.vue
@@ -1,10 +1,31 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li>类型：any</li>
+      <li>
+        描述：
+        切换元素的 CSS 属性 display，
+        实现显示或隐藏元素的效果；
+        <p>
+          <ElSwitch v-model="showTag"/>
+        </p>
+        <p>
+          <ElTag
+            v-show="showTag"
+            :closable="false"
+          >hello
+          </ElTag>
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      showTag: true
+    };
   },
   components: {}
 };
