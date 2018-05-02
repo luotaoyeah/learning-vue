9cd0129f794fa32148708ae20a7d5e0551bdca12
luotao <luotaoyeah@gmail.com>
2018-05-02 21:33:36 +0800

    directives: v-pre

--- src/doc/api/13-directives/11-v-pre.vue
+++ src/doc/api/13-directives/11-v-pre.vue
@@ -1,5 +1,14 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li>
+        跳过对该元素及其子元素的编译；
+        <p v-pre>
+          {{ name }}
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
