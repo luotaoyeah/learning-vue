82279ca11225e1c20a952bf495750ba958ca631b
luotao <luotaoyeah@gmail.com>
2018-05-03 21:47:16 +0800

    options: lifecycle: beforeUpdate

--- src/doc/api/05-options-lifecycle-hooks/05-beforeUpdate.vue
+++ src/doc/api/05-options-lifecycle-hooks/05-beforeUpdate.vue
@@ -1,10 +1,35 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li>
+        数据更改之后，
+        DOM 更新之前；
+      </li>
+      <li>
+        此时，
+        数据已经更改，
+        但尚未重新渲染 DOM；
+        <p>
+          <ElInput
+            v-model="name"
+            style="width: 200px;"
+          />
+          <ElButton>{{name}}</ElButton>
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      name: "tom"
+    };
+  },
+  beforeUpdate() {
+    const vm = this;
+    alert(`name: ${vm.name}`);
   },
   components: {}
 };
