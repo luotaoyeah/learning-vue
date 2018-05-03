6632f0df3f35af872b4fc3d69b3dacae3573d7e1
luotao <luotaoyeah@gmail.com>
2018-05-03 21:15:43 +0800

    options: lifecycle: beforeCreate

--- src/doc/api/05-options-lifecycle-hooks/01-beforeCreate.vue
+++ src/doc/api/05-options-lifecycle-hooks/01-beforeCreate.vue
@@ -1,10 +1,24 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li>
+        组件实例初始化之后立即触发，
+        此时，
+        数据绑定、事件、监视器尚未初始化；
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      name: "tom"
+    };
+  },
+  beforeCreate() {
+    /* undefined */
+    console.log(this.$data);
   },
   components: {}
 };
