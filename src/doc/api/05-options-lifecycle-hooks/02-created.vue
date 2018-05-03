c7caff351262f75c14dcc916efaa30d80bd3f3b4
luotao <luotaoyeah@gmail.com>
2018-05-03 21:23:54 +0800

    options: lifecycle: created

--- src/doc/api/05-options-lifecycle-hooks/02-created.vue
+++ src/doc/api/05-options-lifecycle-hooks/02-created.vue
@@ -1,10 +1,30 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li>
+        组件实例创建完成；
+      </li>
+      <li>
+        此时，
+        所有的组件选项全部处理完成，
+        但是尚未挂载，
+        且 vm.$el 尚未处理；
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      name: "tom"
+    };
+  },
+  created() {
+    /* {"name":"tom"} */
+    console.log(JSON.stringify(this.$data));
+    /* undefined */
+    console.log(this.$el);
   },
   components: {}
 };
