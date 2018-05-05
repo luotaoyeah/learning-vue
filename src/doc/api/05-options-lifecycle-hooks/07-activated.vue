1e3c897d541168e85ea36171d9253ee195cba1a3
luotao <luotaoyeah@gmail.com>
2018-05-05 18:31:53 +0800

    options: lifecycle: activated

--- src/doc/api/05-options-lifecycle-hooks/07-activated.vue
+++ src/doc/api/05-options-lifecycle-hooks/07-activated.vue
@@ -1,12 +1,42 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li>
+        keep-alive 组件激活；
+        <p>
+          <ElButton @click="currentComponent = 'C01'">C01</ElButton>
+          <ElButton @click="currentComponent = 'C02'">C02</ElButton>
+        </p>
+        <p>
+          <keep-alive>
+            <component :is="currentComponent"/>
+          </keep-alive>
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      currentComponent: "C01"
+    };
   },
-  components: {}
+  components: {
+    C01: {
+      template: "<ElTag>C01</ElTag>",
+      activated() {
+        this.$alert("C01.activated()", "activated");
+      }
+    },
+    C02: {
+      template: "<ElTag>C02</ElTag>",
+      activated() {
+        this.$alert("C02.activated()", "activated");
+      }
+    }
+  }
 };
 </script>
 <style scoped>
