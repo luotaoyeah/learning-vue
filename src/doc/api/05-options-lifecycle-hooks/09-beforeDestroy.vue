e8005aebd3b7acd11383b4e9b0cdb60d722e7ca1
luotao <luotaoyeah@gmail.com>
2018-05-05 18:40:36 +0800

    options: lifecycle: beforeDestroy

--- src/doc/api/05-options-lifecycle-hooks/09-beforeDestroy.vue
+++ src/doc/api/05-options-lifecycle-hooks/09-beforeDestroy.vue
@@ -1,12 +1,42 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li>
+        组件实例销毁之前；
+        <p>
+          <ElButton @click="currentComponent = 'C01'">C01</ElButton>
+          <ElButton @click="currentComponent = 'C02'">C02</ElButton>
+        </p>
+        <p>
+          <component :is="currentComponent"/>
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return { currentComponent: "C01" };
   },
-  components: {}
+  components: {
+    C01: {
+      template: "<ElTag>C01</ElTag>",
+      beforeDestroy() {
+        this.$alert("C01.beforeDestroy()", "beforeDestroy", {
+          showClose: false
+        });
+      }
+    },
+    C02: {
+      template: "<ElTag>C02</ElTag>",
+      beforeDestroy() {
+        this.$alert("C02.beforeDestroy()", "beforeDestroy", {
+          showClose: false
+        });
+      }
+    }
+  }
 };
 </script>
 <style scoped>
