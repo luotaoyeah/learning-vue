2e6e50b9c8b2f0ea21c543f1552575d0cfb1b491
luotao <luotaoyeah@gmail.com>
2018-05-06 14:03:12 +0800

    options: assets: filters

--- src/doc/api/06-options-assets/02-filters.vue
+++ src/doc/api/06-options-assets/02-filters.vue
@@ -1,10 +1,42 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li>
+        定义本地过滤器；
+        <p>
+          <ElInput
+            v-model="message"
+            style="width: 200px;"
+          />
+        </p>
+        <p>
+          <ElButton>{{message | upperCaseFilter}}</ElButton>
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
+import Vue from "vue";
+
 export default {
   data() {
-    return {};
+    return {
+      message: "hello"
+    };
+  },
+  filters: {
+    upperCaseFilter(value) {
+      if (!value) {
+        return "";
+      }
+
+      if (!Vue._.isString(value)) {
+        return;
+      }
+
+      return value.toUpperCase();
+    }
   },
   components: {}
 };
