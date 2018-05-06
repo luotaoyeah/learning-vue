040de0c531ab444806035a74672473158a4ed0f0
luotao <luotaoyeah@gmail.com>
2018-05-06 16:38:41 +0800

    options: composition: extends

--- src/doc/api/07-options-composition/03-extends.vue
+++ src/doc/api/07-options-composition/03-extends.vue
@@ -2,17 +2,38 @@
   <div>
     <ul>
       <li>
-        使用声明式的方式，扩展组件；
+        使用声明式的方式，
+        扩展组件；
+        类似 mixins；
+      </li>
+      <li>
+        对应命令式的方式为 Vue.extend()；
       </li>
     </ul>
+    <p>
+      <C02/>
+    </p>
   </div>
 </template>
 <script>
+const C01 = {
+  created() {
+    console.log("C01.created()");
+  }
+};
 export default {
   data() {
     return {};
   },
-  components: {}
+  components: {
+    C02: {
+      template: "<ElTag>C02</ElTag>",
+      created() {
+        console.log("C02.created()");
+      },
+      extends: C01
+    }
+  }
 };
 </script>
 <style scoped>
