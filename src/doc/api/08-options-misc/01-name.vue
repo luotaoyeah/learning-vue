93689a957b7aad74b555f6e4854d118ae11b2f8a
luotao <luotaoyeah@gmail.com>
2018-05-06 21:59:55 +0800

    options: misc: name

--- src/doc/api/08-options-misc/01-name.vue
+++ src/doc/api/08-options-misc/01-name.vue
@@ -1,12 +1,68 @@
 <template>
-  <div><!--TODO--></div>
+  <div>
+    <ul>
+      <li>
+        指定了 name 选项，
+        组件才可以嵌套自己；
+        <component
+          :is="id01"
+          :recursive="true"
+        />
+        <component
+          :is="id02"
+          :recursive="true"
+        />
+      </li>
+      <li>
+        使用 Vue.component() 注册全局组件时，
+        会自动设置 name 选项为组件ID；
+      </li>
+      <li>
+        name 选项不能通过 vm.name 来访问；
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
+import Vue from "vue";
+
+const id01 = Vue._.uniqueId("C_");
+const id02 = Vue._.uniqueId("C_");
+
+Vue.component(id01, {
+  template: `<p><ElTag>${id01}</ElTag><component is="${id01}" v-if="recursive"/></p>`,
+  props: {
+    recursive: {
+      type: Boolean,
+      default: false
+    }
+  },
+  created() {
+    console.log(this.name);
+  }
+});
+
 export default {
   data() {
-    return {};
+    return {
+      id01,
+      id02
+    };
   },
-  components: {}
+  components: {
+    /*
+     * 本地注册的组件，默认没有 name 选项，不能嵌套自己；
+     */
+    [id02]: {
+      template: `<p><ElTag>${id02}</ElTag><component is="${id02}" v-if="recursive"/></p>`,
+      props: {
+        recursive: {
+          type: Boolean,
+          default: false
+        }
+      }
+    }
+  }
 };
 </script>
 <style scoped>
