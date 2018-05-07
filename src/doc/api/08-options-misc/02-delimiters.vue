1d22ed10f23e3a3a886ce9a5b641508b8d02115f
luotao <luotaoyeah@gmail.com>
2018-05-07 21:31:53 +0800

    options: misc: delimiters

--- src/doc/api/08-options-misc/02-delimiters.vue
+++ src/doc/api/08-options-misc/02-delimiters.vue
@@ -1,12 +1,33 @@
 <template>
-  <div><!--TODO--></div>
+  <div>
+    <ul>
+      <li>
+        定义文本解析的分隔符；
+      </li>
+      <li>
+        默认是双花括号：
+        <span v-pre>{{ message }}</span>
+      </li>
+      <p>
+        <C01/>
+      </p>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   data() {
     return {};
   },
-  components: {}
+  components: {
+    C01: {
+      template: "<ElTag>_[ name ]_</ElTag>",
+      delimiters: ["_[", "]_"],
+      data() {
+        return { name: "tom" };
+      }
+    }
+  }
 };
 </script>
 <style scoped>
