48d74a3e5450cfa67ee8adc96e250e49317571ea
luotao <luotaoyeah@gmail.com>
2018-05-07 22:13:13 +0800

    options: misc: inheritAttrs

--- src/doc/api/08-options-misc/05-inheritAttrs.vue
+++ src/doc/api/08-options-misc/05-inheritAttrs.vue
@@ -13,6 +13,9 @@
         依然可以通过 vm.$attrs 访问这些属性，
         并且可以通过 v-bind 将这些属性绑定到任意其他元素上；
       </li>
+      <li>
+        class 和 style 不会受 inheritAttrs 的影响；
+      </li>
     </ul>
     <p>
       <C01
