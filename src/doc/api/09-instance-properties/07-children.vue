806dfeac08c7f36fd8ee2ae7bc36062e72830b65
luotao <luotaoyeah@gmail.com>
2018-05-08 22:08:16 +0800

    instance properties: $children

--- src/doc/api/09-instance-properties/07-children.vue
+++ src/doc/api/09-instance-properties/07-children.vue
@@ -1,12 +1,45 @@
 <template>
-  <div><!--TODO--></div>
+  <div>
+    <ul>
+      <li>
+        组件的直接子组件数组；
+      </li>
+      <li>
+        不能保证子组件的顺序；
+      </li>
+      <li>
+        inreactive 的，
+        不能用于数据绑定；
+      </li>
+    </ul>
+    <p>
+      <C01/>
+    </p>
+    <p>
+      <ElButton @click="handleClick">vm.$children</ElButton>
+    </p>
+  </div>
 </template>
 <script>
 export default {
   data() {
     return {};
   },
-  components: {}
+  methods: {
+    handleClick() {
+      console.log(this.$children);
+    }
+  },
+  components: {
+    C01: {
+      template: "<p class='C01'><C02/></p>",
+      components: {
+        C02: {
+          template: "<ElTag>C02</ElTag>"
+        }
+      }
+    }
+  }
 };
 </script>
 <style scoped>
