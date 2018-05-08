37b86f19bc5d6a4e2bd03071a0ae83faf5e3a2b6
luotao <luotaoyeah@gmail.com>
2018-05-08 21:33:17 +0800

    options: misc: comments

--- src/doc/api/08-options-misc/06-comments.vue
+++ src/doc/api/08-options-misc/06-comments.vue
@@ -1,12 +1,30 @@
 <template>
-  <div><!--TODO--></div>
+  <div>
+    <ul>
+      <li>
+        是否保留模板中的注释，默认不保留；
+        <p>
+          <C01/>
+          <C02/>
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   data() {
     return {};
   },
-  components: {}
+  components: {
+    C01: {
+      template: "<ElButton>C01<!--C01--></ElButton>"
+    },
+    C02: {
+      template: "<ElButton>C01<!--C02--></ElButton>",
+      comments: true
+    }
+  }
 };
 </script>
 <style scoped>
