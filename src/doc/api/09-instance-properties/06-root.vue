67672ba234400646d886ec4ec4b8584b4a8ce000
luotao <luotaoyeah@gmail.com>
2018-05-08 21:52:21 +0800

    instance properties: $root

--- src/doc/api/09-instance-properties/06-root.vue
+++ src/doc/api/09-instance-properties/06-root.vue
@@ -1,10 +1,24 @@
 <template>
-  <div><!--TODO--></div>
+  <div>
+    <ul>
+      <li>
+        当前组件树的根组件；
+        <p>
+          <ElButton @click="handleClick">vm.$root.$el</ElButton>
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   data() {
     return {};
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$root.$el);
+    }
   },
   components: {}
 };
