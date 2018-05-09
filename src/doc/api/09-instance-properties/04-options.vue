60946f2585d174d4b3761bce18257f90ed6ca09a
luotao <luotaoyeah@gmail.com>
2018-05-09 21:45:58 +0800

    instance properties: $options

--- src/doc/api/09-instance-properties/04-options.vue
+++ src/doc/api/09-instance-properties/04-options.vue
@@ -1,12 +1,34 @@
 <template>
-  <div><!--TODO--></div>
+  <div>
+    <ul>
+      <li>
+        组件选项对象，只读；
+      </li>
+      <li>
+        可以用来获取自定义的组件选项；
+        <p>
+          <C01/>
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   data() {
     return {};
   },
-  components: {}
+  components: {
+    C01: {
+      template: "<ElButton @click='handleClick'>C01</ElButton>",
+      age: "tom",
+      methods: {
+        handleClick() {
+          console.log(this.$options.age);
+        }
+      }
+    }
+  }
 };
 </script>
 <style scoped>
