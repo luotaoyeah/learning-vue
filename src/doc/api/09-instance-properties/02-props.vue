e7143e5b8642ba27fa7af1554132def577e385b8
luotao <luotaoyeah@gmail.com>
2018-05-09 21:32:01 +0800

    instance properties: $props

--- src/doc/api/09-instance-properties/02-props.vue
+++ src/doc/api/09-instance-properties/02-props.vue
@@ -1,12 +1,41 @@
 <template>
-  <div><!--TODO--></div>
+  <div>
+    <ul>
+      <li>
+        组件的 props 对象；
+      </li>
+      <li>
+        vm.$props 的属性会被映射到组件实例上，
+        即如果 vm.$props 存在属性 x，
+        则 vm.x === vm.$props.x；
+        <p>
+          <C01 x="tom"/>
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   data() {
     return {};
   },
-  components: {}
+  components: {
+    C01: {
+      template:
+        "<ElButton @click='handleClick'>vm.x === vm.$props.x</ElButton>",
+      props: {
+        x: {
+          type: String
+        }
+      },
+      methods: {
+        handleClick() {
+          console.log("vm.x === vm.$props.x:", this.x === this.$props.x);
+        }
+      }
+    }
+  }
 };
 </script>
 <style scoped>
