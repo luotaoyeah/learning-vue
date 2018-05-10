b75bdbb7bddf20faee98c6b77ffd599f7944b3a0
luotao <luotaoyeah@gmail.com>
2018-05-10 16:44:11 +0800

    instance properties: $scopedSlots

--- src/doc/api/09-instance-properties/09-scopedSlots.vue
+++ src/doc/api/09-instance-properties/09-scopedSlots.vue
@@ -1,12 +1,79 @@
 <template>
-  <div><!--TODO--></div>
+  <div>
+    <ul>
+      <li>
+        组件的内容分发对象（scoped）；
+        <C01 :items="items">
+          <span slot="button">vm.$scopedSlots</span>
+          <template
+            slot="li"
+            slot-scope="{item}"
+          >{{item.id}}. {{item.text}}
+          </template>
+        </C01>
+      </li>
+      <li>
+        如果一个元素声明了 slot-scope 属性，
+        那么这个元素是一个 scopedSlot，
+        包含在 vm.$scopedSlots 对象中；
+      </li>
+      <li>
+        如果一个元素没有声明 slot-scope 属性，
+        那么这个元素是一个普通的 slot，
+        包含在 vm.$slots 对象中；
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      items: [
+        {
+          id: 1,
+          text: "foo"
+        },
+        {
+          id: 2,
+          text: "bar"
+        }
+      ]
+    };
   },
-  components: {}
+  components: {
+    C01: {
+      render(h) {
+        const vm = this;
+        return h("div", [
+          h(
+            "ElButton",
+            {
+              on: {
+                click() {
+                  console.log("vm.slots", vm.$slots);
+                  console.log("vm.$scopedSlots", vm.$scopedSlots);
+                }
+              }
+            },
+            [vm.$slots.button || "C01"]
+          ),
+          h("ul", [
+            vm._.map(vm.items, item =>
+              h("li", [
+                vm.$scopedSlots.li ? vm.$scopedSlots.li({ item }) : item.text
+              ])
+            )
+          ])
+        ]);
+      },
+      props: {
+        items: {
+          type: Array
+        }
+      }
+    }
+  }
 };
 </script>
 <style scoped>
