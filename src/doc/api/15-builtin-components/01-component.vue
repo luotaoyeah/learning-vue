0b5133fedcaef593e7571bc1b9db2f122a83eb9f
luotao <luotaoyeah@gmail.com>
2018-05-12 12:24:48 +0800

    built-in components: component

--- src/doc/api/15-builtin-components/01-component.vue
+++ src/doc/api/15-builtin-components/01-component.vue
@@ -1,12 +1,51 @@
 <template>
-  <div><!--TODO--></div>
+  <div>
+    <ul>
+      <li>
+        用于动态渲染组件；
+      </li>
+      <li>
+        通过 is 属性指定实际渲染的组件；
+        <p>
+          <ElButton @click="currentComponent = 'C01'">C01</ElButton>
+          <ElButton @click="currentComponent = 'C02'">C02</ElButton>
+        </p>
+        <p>
+          <component
+            :is="currentComponent"
+            name="cat"
+          />
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      currentComponent: "C02"
+    };
   },
-  components: {}
+  components: {
+    C01: {
+      template: "<ElButton>{{name}}</ElButton>",
+      props: {
+        name: {
+          type: String
+        }
+      }
+    },
+    C02: {
+      template: "<ElTag>{{name}}</ElTag>",
+      props: {
+        name: {
+          type: String,
+          default: "tom"
+        }
+      }
+    }
+  }
 };
 </script>
 <style scoped>
