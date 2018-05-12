4c06d3e36698d0be53961d5f5c4044c7391ad1f5
luotao <luotaoyeah@gmail.com>
2018-05-12 09:27:37 +0800

    instance methods: $destroy

--- src/doc/api/12-instance-methods-lifecycle/02-forceUpdate.vue
+++ src/doc/api/12-instance-methods-lifecycle/02-forceUpdate.vue
@@ -1,10 +1,33 @@
 <template>
-  <div><!--TODO--></div>
+  <div>
+    <ul>
+      <li>
+        强制组件重新渲染；
+        <p>
+          <ElInput
+            v-model="x"
+            style="width: 100px;"
+          />
+          <ElButton>{{x}}</ElButton>
+          <ElButton @click="handleClick">vm.$forceUpdate</ElButton>
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   data() {
     return {};
+  },
+  created() {
+    this.x = "x";
+  },
+  methods: {
+    handleClick() {
+      const vm = this;
+      vm.$forceUpdate();
+    }
   },
   components: {}
 };
