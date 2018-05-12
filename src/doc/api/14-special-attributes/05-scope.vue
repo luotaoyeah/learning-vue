6fc2f35704970056b6fb693ce38f6a30c94ce151
luotao <luotaoyeah@gmail.com>
2018-05-12 11:20:52 +0800

    special attributes: scope

--- src/doc/api/14-special-attributes/05-scope.vue
+++ src/doc/api/14-special-attributes/05-scope.vue
@@ -2,7 +2,17 @@
   <div>
     <ul>
       <li>
-
+        已弃用，使用 slot-scope 代替；
+      </li>
+      <li>
+        用法跟 slot-scope 一样，但是只能用在 template 元素上面；
+        <C01 :items="items">
+          <template scope="{ item }">
+            <p>
+              <ElButton>{{item.id}}. {{item.text}}</ElButton>
+            </p>
+          </template>
+        </C01>
       </li>
     </ul>
   </div>
@@ -10,9 +20,34 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      items: [
+        {
+          id: 1,
+          text: "foo"
+        },
+        {
+          id: 2,
+          text: "bar"
+        }
+      ]
+    };
   },
-  components: {}
+  components: {
+    C01: {
+      template: `
+        <ul>
+          <li v-for="item in items" :key="item.id">
+            <slot :item="item">{{item.text}}</slot>
+          </li>
+        </ul>`,
+      props: {
+        items: {
+          type: Array
+        }
+      }
+    }
+  }
 };
 </script>
 <style scoped>
