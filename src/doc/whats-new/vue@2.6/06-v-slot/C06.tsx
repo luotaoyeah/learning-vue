203bf05c62c97624ce484e290d922c74ab3b12eb
luotao <luotaoyeah@gmail.com>
2019-02-12 20:36:24 +0800

    what's new: vue@2.6: shorthand syntax for v-slot

--- src/doc/whats-new/vue@2.6/06-v-slot/C06.tsx
+++ src/doc/whats-new/vue@2.6/06-v-slot/C06.tsx
@@ -4,6 +4,7 @@ import { Tabs, TabPane } from "element-ui";
 import C0601 from "./C0601.vue";
 import C0602 from "./C0602.vue";
 import C0603 from "./C0603.vue";
+import C0604 from "./C0604.vue";
 
 @Component({})
 export default class C06 extends Vue {
@@ -18,6 +19,9 @@ export default class C06 extends Vue {
         </TabPane>
         <TabPane label="default slot">
           <C0603 />
+        </TabPane>
+        <TabPane label="shorthand for v-slot">
+          <C0604 />
         </TabPane>
       </Tabs>
     );
--- src/doc/whats-new/vue@2.6/06-v-slot/C0601.vue
+++ src/doc/whats-new/vue@2.6/06-v-slot/C0601.vue
@@ -35,7 +35,8 @@ import Vue, { CreateElement, RenderContext, VNode } from "vue";
 
 /*
  * vue@2.6 引入了一个新的指令 v-slot，
- * 用来统一 scoped-slot 和 normal-slot
+ * 用来统一 scoped-slot 和 normal-slot，
+ * v-slot 只能用在组件标签或者 <template> 标签上，不能用在普通的 HTML 标签上
  */
 
 const C01 = Vue.component("C01", {
