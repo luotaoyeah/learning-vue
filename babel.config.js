46167ad4361ee04241395e5926f67a2e4172aa1a
luotao <luotaoyeah@gmail.com>
2019-07-05 18:01:29 +0800

    feat: adopt ant-design-pro-vue
    #1

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,20 @@
 module.exports = {
-  presets: ["@vue/app"],
-  plugins: [
+  presets: [
+    '@vue/app',
     [
-      "import",
-      { libraryName: "ant-design-vue", libraryDirectory: "es", style: true }
+      '@babel/preset-env',
+      {
+        'useBuiltIns': 'entry'
+      }
     ]
   ]
-};
+  // if your use import on Demand, Use this code
+  // ,
+  // plugins: [
+  //   [ 'import', {
+  //     'libraryName': 'ant-design-vue',
+  //     'libraryDirectory': 'es',
+  //     'style': true // `style: true` 会加载 less 文件
+  //   } ]
+  // ]
+}
--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  moduleFileExtensions: ["js", "jsx", "json", "vue", "ts", "tsx"],
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'json',
+    'vue'
+  ],
   transform: {
-    "^.+\\.vue$": "vue-jest",
-    ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
-      "jest-transform-stub",
-    "^.+\\.tsx?$": "ts-jest"
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.jsx?$': 'babel-jest'
   },
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1"
+    '^@/(.*)$': '<rootDir>/src/$1'
   },
-  snapshotSerializers: ["jest-serializer-vue"],
-  testMatch: ["**/*.test.(js|jsx|ts|tsx)"],
-  testURL: "http://localhost/",
-  collectCoverage: true,
-  // FIXME 在 circleci 环境执行测试, 会抛出 ENOMEM 错误
-  // collectCoverageFrom: ["**/*.{ts,tsx}", "!**/node_modules/**"],
-  collectCoverageFrom: [
-    "src/app/doc/api/01-global-config/06-ignored-elements/C0106.tsx",
-    "!**/node_modules/**"
+  snapshotSerializers: [
+    'jest-serializer-vue'
   ],
-  coverageReporters: ["lcovonly", "text-summary"]
-};
+  testMatch: [
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
+  ],
+  testURL: 'http://localhost/'
+}
