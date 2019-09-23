34051a570336e4901cc57da26de29c935af415de
luotao <luotaoyeah@gmail.com>
2019-09-23 18:32:31 +0800

    refactor: re-create using @vue/cli@4.2.3

--- src/views/exception/c404.tsx
+++
@@ -1,9 +0,0 @@
-import { Component, Vue } from "vue-property-decorator";
-import { ExceptionPage } from "@/components";
-
-@Component({})
-export default class C404 extends Vue {
-  public render() {
-    return <ExceptionPage type="404" />;
-  }
-}
