2295bd4befcee90cb717ee063f22a096c855aaf3
luotao <luotaoyeah@gmail.com>
2020-02-17 18:03:21 +0800

    feat(guide): component-basics.html#emitting-a-value-with-an-event

---
+++ src/views/doc/guide/component-basics/listening-to-child-components-events.02.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="$emit('event-01', 1, 2, 3)">CLICK</button>
+</template>
+
+<script>
+// https://v3.vuejs.org/guide/component-basics.html#emitting-a-value-with-an-event
+
+export default {};
+</script>
+
+<style lang="less" scoped>
+button {
+  border-radius: 3px;
+  margin-right: 12px;
+}
+</style>
--- src/views/doc/guide/component-basics/listening-to-child-components-events.vue
+++ src/views/doc/guide/component-basics/listening-to-child-components-events.vue
@@ -8,12 +8,22 @@
       @change-color="onChangeColor"
     ></comp-01>
   </fieldset>
+
+  <fieldset>
+    <legend>EMITTING A VALUE WITH AN EVENT</legend>
+    <comp-02 @event-01="onEvent01"></comp-02>
+    <!------------------------------------------------------------------------------------------------------
+        监听函数使用 inline function 时，如何获取多个参数？
+    ------------------------------------------------------------------------------------------------------->
+    <comp-02 @event-01="(x, y, z) => onEvent01(x, y, z)"></comp-02>
+  </fieldset>
 </template>
 
 <script>
 // https://v3.vuejs.org/guide/component-basics.html#listening-to-child-components-events
 
 import comp01 from './listening-to-child-components-events.01';
+import comp02 from './listening-to-child-components-events.02';
 
 export default {
   data() {
@@ -30,9 +40,15 @@ export default {
     onChangeColor() {
       this.color = 'blue';
     },
+    onEvent01(a, b, c) {
+      console.assert(a === 1);
+      console.assert(b === 2);
+      console.assert(c === 3);
+    },
   },
   components: {
     'comp-01': comp01,
+    'comp-02': comp02,
   },
 };
 </script>
