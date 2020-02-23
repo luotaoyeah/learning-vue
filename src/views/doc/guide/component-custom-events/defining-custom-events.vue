c999eb41217901f53ace4c27e3fcab11f7836158
luotao <luotaoyeah@gmail.com>
2020-02-23 11:30:20 +0800

    feat(guide): component-custom-events.html#validate-emitted-events

--- src/views/doc/guide/component-custom-events/defining-custom-events.vue
+++ src/views/doc/guide/component-custom-events/defining-custom-events.vue
@@ -1,6 +1,7 @@
 <template>
   <fieldset>
     <comp-01 @event-01="onEvent01" @event-03="onEvent01"></comp-01>
+    <comp-02 @event-02="onEvent02"></comp-02>
   </fieldset>
 </template>
 
@@ -8,15 +9,20 @@
 // https://v3.vuejs.org/guide/component-custom-events.html#defining-custom-events
 
 import comp01 from './defining-custom-events.01';
+import comp02 from './defining-custom-events.02';
 
 export default {
   methods: {
     onEvent01() {
       console.log(666);
     },
+    onEvent02() {
+      console.log(888);
+    },
   },
   components: {
     'comp-01': comp01,
+    'comp-02': comp02,
   },
 };
 </script>
