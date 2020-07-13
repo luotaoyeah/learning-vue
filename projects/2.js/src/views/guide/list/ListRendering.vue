e03cdb8b8e54d419eb5c9d34077bb2a18afcfa72
luotao <luotaoyeah@gmail.com>
2020-07-13 21:48:47 +0800

    feat(2.js): https://v2.vuejs.org/v2/guide/list.html#Mutation-Methods

--- projects/2.js/src/views/guide/list/ListRendering.vue
+++ projects/2.js/src/views/guide/list/ListRendering.vue
@@ -7,6 +7,32 @@
         <li v-for="(value, key, index) in obj01">[{{ index }}] [{{ key }}] [{{ value }}]</li>
       </ul>
     </fieldset>
+
+    <fieldset>
+      <legend>Array Change Detection</legend>
+
+      <p>vue 对下面这些直接修改 array 的方法进行了包装, 从而可以监测到对 array 的修改.</p>
+
+      <p>
+        <button @click="push()">push()</button>
+      </p>
+      <p>
+        <button @click="pop()">pop()</button>
+      </p>
+      <p>
+        <button @click="shift()">shift()</button>
+      </p>
+      <p>
+        <button @click="unshift()">unshift()</button>
+      </p>
+      <p>
+        <button @click="reverse()">reverse()</button>
+      </p>
+
+      <ul>
+        <li v-for="value in items01" :key="value">{{ value }}</li>
+      </ul>
+    </fieldset>
   </div>
 </template>
 
@@ -20,7 +46,25 @@ export default {
         y: 'Y',
         z: 'Z',
       },
+      items01: [1, 2, 3],
     };
+  },
+  methods: {
+    push() {
+      this.items01.push(this.items01[this.items01.length - 1] + 1);
+    },
+    pop() {
+      this.items01.pop();
+    },
+    shift() {
+      this.items01.shift();
+    },
+    unshift() {
+      this.items01.unshift(this.items01[0] - 1);
+    },
+    reverse() {
+      this.items01.reverse();
+    },
   },
 };
 </script>
