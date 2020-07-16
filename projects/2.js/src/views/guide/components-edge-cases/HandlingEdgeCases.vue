62fe27e837ae72b63829ba23d935d21b89e2ae78
luotao <luotaoyeah@gmail.com>
2020-07-16 22:05:31 +0800

    feat(2.js): https://v2.vuejs.org/v2/guide/components-edge-cases.html#Accessing-Child-Component-Instances-amp-Child-Elements

--- projects/2.js/src/views/guide/components-edge-cases/HandlingEdgeCases.vue
+++ projects/2.js/src/views/guide/components-edge-cases/HandlingEdgeCases.vue
@@ -5,6 +5,15 @@
 
       <p>通过 vm.$root 访问在 main.js 中通过 new Vue() 创建的实例.</p>
     </fieldset>
+
+    <fieldset>
+      <legend>Accessing Child Component Instances & Child Elements</legend>
+
+      <p>
+        <comp-a ref="compaRef"></comp-a>
+        <button @click="focusCompA">focus comp-a</button>
+      </p>
+    </fieldset>
   </div>
 </template>
 
@@ -13,6 +22,26 @@ export default {
   name: 'HandlingEdgeCases',
   mounted() {
     console.log(this.$root.foo);
+  },
+  methods: {
+    focusCompA() {
+      this.$refs.compaRef.focus();
+    },
+  },
+  components: {
+    CompA: {
+      template: `
+        <input type='text' ref='inputRef'>
+      `,
+      data() {
+        return {};
+      },
+      methods: {
+        focus() {
+          this.$refs.inputRef.focus();
+        },
+      },
+    },
   },
 };
 </script>
