c1dd0c627724688a6ea5855ec97f4e2367bca65e
luotao <luotaoyeah@gmail.com>
2020-07-17 20:19:10 +0800

    feat(2.js): https://v2.vuejs.org/v2/guide/components-edge-cases.html#Dependency-Injection

--- projects/2.js/src/views/guide/components-edge-cases/HandlingEdgeCases.vue
+++ projects/2.js/src/views/guide/components-edge-cases/HandlingEdgeCases.vue
@@ -14,6 +14,14 @@
         <button @click="focusCompA">focus comp-a</button>
       </p>
     </fieldset>
+
+    <fieldset>
+      <legend>Dependency Injection</legend>
+
+      <p>provide / inject 共同提供了 DI 的功能.</p>
+
+      <comp-b></comp-b>
+    </fieldset>
   </div>
 </template>
 
@@ -21,12 +29,18 @@
 export default {
   name: 'HandlingEdgeCases',
   mounted() {
-    console.log(this.$root.foo);
+    console.log('this.$root.foo', this.$root.foo);
   },
   methods: {
     focusCompA() {
       this.$refs.compaRef.focus();
     },
+  },
+  provide() {
+    return {
+      p01: 1,
+      p02: 2,
+    };
   },
   components: {
     CompA: {
@@ -40,6 +54,24 @@ export default {
         focus() {
           this.$refs.inputRef.focus();
         },
+      },
+    },
+    CompB: {
+      template: `
+        <p>CompB</p>
+      `,
+      data() {
+        return {};
+      },
+      inject: {
+        p01: 'p01',
+        p02: { from: 'p02', default: 2 },
+        p03: { default: 3 },
+      },
+      mounted() {
+        console.log('this.p01', this.p01);
+        console.log('this.p02', this.p02);
+        console.log('this.p03', this.p03);
       },
     },
   },
