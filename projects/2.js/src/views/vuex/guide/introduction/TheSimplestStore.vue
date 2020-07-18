2ff35aafe107389d2ae0b083943f6d3ac7d697bb
luotao <luotaoyeah@gmail.com>
2020-07-18 19:12:15 +0800

    feat(2.js): https://v3.vuex.vuejs.org/guide/#the-simplest-store

---
+++ projects/2.js/src/views/vuex/guide/introduction/TheSimplestStore.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <fieldset>
+      <legend></legend>
+
+      <p>{{ n }}</p>
+      <p>
+        <button @click="onClick">n++</button>
+      </p>
+    </fieldset>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheSimplestStore',
+  // 通过 computed property 获取一个 state 数据
+  computed: {
+    n() {
+      return this.$store.state.count;
+    },
+  },
+  methods: {
+    onClick() {
+      // 通过 vm.$store.commit('SOME_MUTATION') 触发一个 mutation 方法执行, 从而修改 state 数据
+      this.$store.commit('INCREMENT');
+    },
+  },
+};
+</script>
+
+<style scoped></style>
