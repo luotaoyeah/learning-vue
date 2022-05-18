2b4187bf69cd23a3649697dfd7933cbaa0cf1180
luotao <luotaoyeah@gmail.com>
2022-05-18 23:03:08 +0800

    feat(3): https://vuejs.org/guide/essentials/template-syntax.html#raw-html

--- projects/3/src/router/index.ts
+++ projects/3/src/router/index.ts
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/guide/essentials/template-syntax',
+      name: 'guide/essentials/template-syntax',
+      component: () => import('../views/guide/essentials/TemplateSyntax.vue'),
+    },
   ],
 });
 
---
+++ projects/3/src/views/guide/essentials/TemplateSyntax.vue
@@ -0,0 +1,26 @@
+<template>
+  <fieldset>
+    <legend>text-interpolation</legend>
+
+    <div>{{ msg }}</div>
+  </fieldset>
+
+  <fieldset>
+    <legend>raw-html</legend>
+
+    <div>{{ rawHtml }}</div>
+    <div v-html="rawHtml"></div>
+  </fieldset>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+// http://localhost:8888/guide/essentials/template-syntax
+
+const msg = ref('HELLO WORLD');
+
+const rawHtml = ref<string>("<span style='color:red;'>红色</span>");
+</script>
+
+<style scoped></style>
