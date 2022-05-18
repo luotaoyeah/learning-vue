16827e610cfe75a53d89b5a9b3997e994e456826
luotao <luotaoyeah@gmail.com>
2022-05-18 22:35:03 +0800

    feat(3): https://vuejs.org/guide/essentials/application.html
    https://vuejs.org/api/application.html#createapp

--- projects/3/.eslintrc.cjs
+++ projects/3/.eslintrc.cjs
@@ -1,5 +1,5 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
@@ -10,6 +10,7 @@ module.exports = {
     '@vue/eslint-config-prettier'
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
--- projects/3/src/main.ts
+++ projects/3/src/main.ts
@@ -1,14 +1,12 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/main.css";
 
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
 
-import './assets/main.css'
+app.use(createPinia());
+app.use(router);
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+app.mount("#app");
