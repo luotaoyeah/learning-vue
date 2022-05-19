ef406205c6c781f1e9158d3c1036cd3489dedd4f
luotao <luotaoyeah@gmail.com>
2022-05-19 20:56:39 +0800

    feat(3): https://vuejs.org/guide/essentials/template-syntax.html#attribute-bindings

--- projects/3/src/views/guide/essentials/TemplateSyntax.vue
+++ projects/3/src/views/guide/essentials/TemplateSyntax.vue
@@ -11,6 +11,17 @@
     <div>{{ rawHtml }}</div>
     <div v-html="rawHtml"></div>
   </fieldset>
+
+  <fieldset>
+    <legend>attribute-bindings</legend>
+
+    <div v-bind:data-foo="msg">1</div>
+    <div :data-foo="msg" :data-bar="null" :data-baz="undefined">2</div>
+
+    <button :disabled="''">button</button>
+
+    <div v-bind="attrs01" :data-baz="3">3</div>
+  </fieldset>
 </template>
 
 <script lang="ts" setup>
@@ -21,6 +32,11 @@ import { ref } from 'vue';
 const msg = ref('HELLO WORLD');
 
 const rawHtml = ref<string>("<span style='color:red;'>红色</span>");
+
+const attrs01 = ref({
+  'data-foo': 1,
+  'data-bar': 2,
+});
 </script>
 
 <style scoped></style>
