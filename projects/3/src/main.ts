afd967b0ce138bc0c0dd1c7a73c9e17dc0ce5d27
luotao <luotaoyeah@gmail.com>
2022-05-19 21:18:17 +0800

    feat(3): https://vuejs.org/guide/essentials/template-syntax.html#using-javascript-expressions

--- projects/3/src/main.ts
+++ projects/3/src/main.ts
@@ -1,12 +1,14 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import App from "./App.vue";
-import router from "./router";
-import "./assets/main.css";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
+import './assets/main.css';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
-app.mount("#app");
+app.config.globalProperties.$log = console.log;
+
+app.mount('#app');
--- projects/3/src/views/guide/essentials/TemplateSyntax.vue
+++ projects/3/src/views/guide/essentials/TemplateSyntax.vue
@@ -16,11 +16,38 @@
     <legend>attribute-bindings</legend>
 
     <div v-bind:data-foo="msg">1</div>
+
+    <!--绑定值为 null/undefined 时, 属性会被移除-->
     <div :data-foo="msg" :data-bar="null" :data-baz="undefined">2</div>
 
+    <!--对于 boolean attribute, 空字符串会被当成 truthy 值-->
     <button :disabled="''">button</button>
 
+    <!--v-bind 后面不加 :some-attr, 表示同时绑定多个属性-->
     <div v-bind="attrs01" :data-baz="3">3</div>
+  </fieldset>
+
+  <fieldset>
+    <legend>using-javascript-expressions</legend>
+
+    <!--mustache 和 v-bind 的绑定值可以是任意合法的表达式-->
+    <!--例如, IIFE 也是表达式-->
+    <div>
+      {{
+        (function () {
+          return 'HELLO';
+        })()
+      }}
+
+      <!--可访问的全局对象是受限的, 但是可以通过 app.config.globalProperties.xxx 自行扩展-->
+      {{ (() => Date.now())() }}
+      {{
+        (() => {
+          $log('FOO');
+          return '';
+        })()
+      }}
+    </div>
   </fieldset>
 </template>
 
