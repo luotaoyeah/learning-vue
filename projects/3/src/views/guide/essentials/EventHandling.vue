631fa25c3830f63a00668d1f18be7195825b7e66
luotao <luotaoyeah@gmail.com>
2022-05-21 20:15:23 +0800

    feat(3): https://vuejs.org/guide/essentials/event-handling.html#key-modifiers

--- projects/3/src/views/guide/essentials/EventHandling.vue
+++ projects/3/src/views/guide/essentials/EventHandling.vue
@@ -32,6 +32,34 @@
     <button @click="fn02('foo', $event)">CLICK ME</button>
     <button @click="(event) => fn02('bar', event)">CLICK ME</button>
   </fieldset>
+
+  <fieldset>
+    <legend>event-modifiers</legend>
+
+    <!--可以只使用 event modifier, 而不绑定处理函数-->
+    <button @click.prevent>CLICK ME</button>
+  </fieldset>
+
+  <fieldset>
+    <legend>key-modifiers</legend>
+
+    <!--
+        任何合法的 KeyboardEvent.key 值都可以作为键盘事件的 modifier,
+        注意, 要将 KeyboardEvent.key 转换为 kebab-case
+    -->
+    <input @keyup.escape="fn03('escape')" />
+
+    <!--对于一些常用的键, vue 给他们设置了别名, 方便我们使用, 比如 escape 的别名为 esc-->
+    <input @keyup.esc="fn03('esc')" />
+
+    <!--
+        .exact 跟控制键(ctrl/shift/alt/meat)一起使用,
+        表示, 只有指定的控制键按下, 没有其他的控制键按下时, 才会触发事件,
+        如下, 只有 shift 按下, ctrl/alt/meta 都没有按下时才会触发事件
+    -->
+    <input @keyup.shift.exact="fn04()" />
+    <input @keyup.shift="fn05()" />
+  </fieldset>
 </template>
 
 <script lang="ts" setup>
@@ -49,6 +77,18 @@
 
   function fn02(message: string, event: MouseEvent) {
     console.log(`${event.type}, ${message}`);
+  }
+
+  function fn03(key: string) {
+    console.log(key);
+  }
+
+  function fn04() {
+    console.log('.exact');
+  }
+
+  function fn05() {
+    console.log('without .exact');
   }
 </script>
 
