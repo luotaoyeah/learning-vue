72ecc0342028aa8fd1f289d2f17f6fdca5ced87f
luotao <luotaoyeah@gmail.com>
2022-05-22 19:49:30 +0800

    feat(3): https://vuejs.org/guide/components/registration.html#component-name-casing

---
+++ projects/3/src/components/CompF.vue
@@ -0,0 +1,9 @@
+<template>
+  <fieldset>
+    <legend>F</legend>
+  </fieldset>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
--- projects/3/src/views/guide/components/Registration.vue
+++ projects/3/src/views/guide/components/Registration.vue
@@ -1,13 +1,26 @@
 <template>
   <fieldset>
-    <legend>binding-html-classes</legend>
+    <legend>global-registration</legend>
 
     <CompE></CompE>
+  </fieldset>
+
+  <fieldset>
+    <legend>component-name-casing</legend>
+
+    <!--注册组件时使用 PascalCase 命名, 使用组件时 PascalCase 和 kebab-case 都可以-->
+
+    <CompE></CompE>
+    <comp-e></comp-e>
+
+    <CompF></CompF>
+    <comp-f></comp-f>
   </fieldset>
 </template>
 
 <script lang="ts" setup>
   // http://localhost:8888/guide/components/registration
+  import CompF from '@/components/CompF.vue';
 </script>
 
 <style lang="css" scoped></style>
