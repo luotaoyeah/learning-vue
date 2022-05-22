8176dfa8ec9c6e99aa4485c81c1d1b795a692537
luotao <luotaoyeah@gmail.com>
2022-05-22 22:27:32 +0800

    feat(3): https://vuejs.org/guide/components/props.html#boolean-casting

--- projects/3/src/components/CompG.vue
+++ projects/3/src/components/CompG.vue
@@ -2,7 +2,10 @@
   <fieldset>
     <legend>G</legend>
 
-    <div>{{ tName }}, {{ tAge }}</div>
+    <div>{{ String(tName) }}, {{ tAge }}</div>
+
+    <div>tA: {{ tA }}</div>
+    <div>tB: {{ String(tB) }}</div>
   </fieldset>
 </template>
 
@@ -13,13 +16,28 @@
   // defineProps(['tName']);
 
   defineProps({
-    /** 姓名. */
     tName: String,
-    /** 年龄. */
     tAge: {
       type: Number,
-      required: true,
       default: 18,
+      validator(value: unknown): boolean {
+        return (value as number) <= 20;
+      },
+    },
+
+    // 类型为 Object 时, default 需要定义成一个 factory function
+    tA: {
+      type: Object,
+      default(props: any) {
+        console.log('props:', props);
+        return { x: 1 };
+      },
+    },
+
+    // Boolean 类型的 prop 的默认值为 false, 除非使用 default 指定了其他的默认值
+    // 其他类型的 prop 的默认值为 undefined, 除非使用 default 指定了其他的默认值
+    tB: {
+      type: Boolean,
     },
   });
 </script>
--- projects/3/src/views/guide/components/Props.vue
+++ projects/3/src/views/guide/components/Props.vue
@@ -2,7 +2,7 @@
   <fieldset>
     <legend>props-declaration</legend>
 
-    <CompG t-name="luotao" :t-age="20"></CompG>
+    <CompG :t-age="20" t-name="luotao"></CompG>
   </fieldset>
 
   <fieldset>
@@ -12,8 +12,22 @@
         声明 props 时通常使用 camelCase,
         传递 props 时通常使用 kebab-case, 当然 camelCase 也是支持的
     -->
-    <CompG t-name="luotao" :t-age="20"></CompG>
-    <CompG tName="luotao" :tAge="20"></CompG>
+    <CompG :t-age="20" t-name="luotao"></CompG>
+    <CompG :tAge="20" tName="luotao"></CompG>
+  </fieldset>
+
+  <fieldset>
+    <legend>prop-validation</legend>
+
+    <CompG :t-age="22"></CompG>
+
+    <!--
+        Boolean 类型的 prop 类似于 Boolean 类型的 attribute,
+        present 时表示传值为 true,
+        absent  时表示传值为 undefined
+    -->
+    <CompG t-b></CompG>
+    <CompG :t-b="true"></CompG>
   </fieldset>
 </template>
 
