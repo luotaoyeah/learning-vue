34fd51c3bf30ed854165f55ce851ac3d6a99fa79
luotao <luotaoyeah@gmail.com>
2022-05-22 17:25:10 +0800

    feat(3): https://vuejs.org/guide/essentials/template-refs.html#function-refs

--- projects/3/src/views/guide/essentials/TemplateRefs.vue
+++ projects/3/src/views/guide/essentials/TemplateRefs.vue
@@ -4,6 +4,15 @@
 
     <div ref="div01">A</div>
   </fieldset>
+
+  <fieldset>
+    <legend>function-refs</legend>
+
+    <div>
+      <p><input v-if="visible" type="text" :ref="setRef01" /></p>
+      <button @click="visible = !visible">toggle</button>
+    </div>
+  </fieldset>
 </template>
 
 <script lang="ts" setup>
@@ -16,6 +25,14 @@
   watch(div01, (newValue, oldValue) => {
     console.log(`div01:`, newValue);
   });
+
+  // --------------------------------------------------
+  const visible = ref<boolean>(true);
+
+  // :ref 可以绑定一个函数, 函数第一个参数就是元素的实例对象,
+  function setRef01(el: HTMLInputElement | null) {
+    console.log('el:', el);
+  }
 </script>
 
 <style lang="css" scoped></style>
