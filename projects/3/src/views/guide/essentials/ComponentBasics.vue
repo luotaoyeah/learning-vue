6b3a0eea3b2545975e70afd0d9d41415b78afe8c
luotao <luotaoyeah@gmail.com>
2022-05-22 18:49:39 +0800

    feat(3): https://vuejs.org/guide/essentials/component-basics.html#dynamic-components

--- projects/3/src/views/guide/essentials/ComponentBasics.vue
+++ projects/3/src/views/guide/essentials/ComponentBasics.vue
@@ -11,14 +11,34 @@
 
     <CompD @t-click="fn01"></CompD>
   </fieldset>
+
+  <fieldset>
+    <legend>dynamic-components</legend>
+
+    <component :is="comp01" :style="style01" v-html="'FOO'"></component>
+    <button @click="toggle">toggle</button>
+  </fieldset>
 </template>
 
 <script lang="ts" setup>
   // http://localhost:8888/guide/essentials/component-basics
   import CompD from '@/components/CompD.vue';
+  import { ref } from 'vue';
 
   function fn01() {
     console.log('t-click');
+  }
+
+  // --------------------------------------------------
+  const comp01 = ref<'span' | 'div'>('span');
+  const style01 = { border: '1px solid #000', 'border-radius': '3px', padding: '3px 3px 2px 3px' };
+
+  function toggle() {
+    if (comp01.value === 'span') {
+      comp01.value = 'div';
+    } else {
+      comp01.value = 'span';
+    }
   }
 </script>
 
