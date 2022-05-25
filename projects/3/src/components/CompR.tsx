79dd3fb95d78d7549052bff4f7e6fc55d551cfc3
luotao <luotaoyeah@gmail.com>
2022-05-25 22:27:02 +0800

    feat(3): https://vuejs.org/guide/extras/render-function.html#jsx-tsx

---
+++ projects/3/src/components/CompR.tsx
@@ -0,0 +1,9 @@
+import { defineComponent } from 'vue';
+
+const CompR = defineComponent({
+  setup() {
+    return () => <div>R</div>;
+  },
+});
+
+export { CompR };
--- projects/3/src/views/guide/extras/RenderFunction.vue
+++ projects/3/src/views/guide/extras/RenderFunction.vue
@@ -4,12 +4,19 @@
 
     <CompA></CompA>
   </fieldset>
+
+  <fieldset>
+    <legend>jsx-tsx</legend>
+
+    <CompR></CompR>
+  </fieldset>
 </template>
 
 <script lang="ts" setup>
   // http://localhost:8888/guide/extras/render-function
 
   import { defineComponent, h } from 'vue';
+  import { CompR } from '@/components/CompR';
 
   const CompA = defineComponent({
     setup() {
