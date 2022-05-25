558204b96d9d32d0b4adc3685916d1b5630c5ad1
luotao <luotaoyeah@gmail.com>
2022-05-25 23:18:25 +0800

    feat(3): https://router.vuejs.org/guide/essentials/route-matching-syntax.html#custom-regex-in-params

---
+++ projects/3/src/views/vue-router/guide/essentials/CustomRegexInParams.vue
@@ -0,0 +1,17 @@
+<template>
+    <fieldset>
+        <legend>custom-regex-in-params</legend>
+
+        <div>{{ route.params.id }}</div>
+    </fieldset>
+</template>
+
+<script lang="ts" setup>
+    // http://localhost:8888/vue-router/guide/essentials/route-matching-syntax/custom-regex-in-params/9
+
+    import { useRoute } from 'vue-router';
+
+    const route = useRoute();
+</script>
+
+<style scoped></style>
