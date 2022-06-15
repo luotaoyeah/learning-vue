653efc9329a49f1325e5fc64d7444fd4c314f003
luotao <luotaoyeah@gmail.com>
2022-06-15 22:10:20 +0800

    feat(tinymce-vue): v-model

--- projects/packages/tinymce-vue/README.md
+++ projects/packages/tinymce-vue/README.md
@@ -1,2 +1,5 @@
 # @luotao/learning-vue.tinymce-vue
 
+[https://www.tiny.cloud/docs/tinymce/6/vue-cloud/](https://www.tiny.cloud/docs/tinymce/6/vue-cloud/)
+
+[https://www.tiny.cloud/docs/tinymce/6/vue-ref/](https://www.tiny.cloud/docs/tinymce/6/vue-ref/)
--- projects/packages/tinymce-vue/src/App.vue
+++ projects/packages/tinymce-vue/src/App.vue
@@ -1,7 +1,32 @@
 <template>
-    <div>HELLO</div>
+    <Editor
+        v-model="content"
+        :init="{
+            plugins: 'lists link image table code help wordcount',
+        }"
+        api-key="no-api-key"
+    ></Editor>
+
+    <fieldset>
+        <legend></legend>
+        {{ content }}
+    </fieldset>
+
+    <fieldset>
+        <legend></legend>
+        <div v-html="content"></div>
+    </fieldset>
 </template>
 
-<script setup></script>
+<script setup>
+    import Editor from '@tinymce/tinymce-vue';
+    import { ref, watchEffect } from 'vue';
+
+    const content = ref('');
+
+    watchEffect(() => {
+        console.log(content.value);
+    });
+</script>
 
 <style scoped></style>
